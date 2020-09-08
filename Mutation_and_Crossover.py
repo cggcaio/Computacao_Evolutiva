9a813447bd12a6036b6444b26a88e2f8010268b8
Caio Gonçalves <cggcaio2014@gmail.com>
2020-09-08 13:52:20 -0300

    Up Crossover

--- Mutation_and_Crossover.py
+++ Mutation_and_Crossover.py
@@ -11,9 +11,16 @@ mutation_prob = 0.2
 crossover_prob = 1 - 0.2
 
 r = random.randint(0,1)
+r = 0.3
+n_permutacoes = 1
+print('P1 Original: ', parent1, '\n')
+print('P2 Original: ', parent2, '\n\n')
 
-n_permutacoes = 40
-print(parent1)
+if (r < mutation_prob):
+  print('----------- Mutation ----------')
+else:
+  print('----------- Crossover -----------')
+
 for n in range(n_permutacoes):
   if (r < mutation_prob): #gerar filho com mutacao
     dna = random.choice(range(len(parent1)))
@@ -21,10 +28,15 @@ for n in range(n_permutacoes):
       parent1[dna]=False
     else:
       parent1[dna]=True
+    print('Mutation: ', parent1)
   else: # gerar filho com crossover
-    print('Crossover')
-
-print(parent1)
+    point = random.randint(0,51)
+    print('Crossover Point: ', point)
+    kid1=parent2[:1]+parent1[1:]
+    kid2=parent1[:1]+parent2[1:]
+    
+    print('Kid1: ', kid1)
+    print('Kid2: ', kid2)
 
 
 #print(parent1)
